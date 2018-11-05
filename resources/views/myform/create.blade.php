fbcb53ed5d0f8022d9b9e3ba54c2d0d272441f6b
Bhar, Arnab <Arnab.Bhar@ext.us.panasonic.com>
2018-11-04 21:44:14 -0500

    fix some w3 validator log

--- resources/views/myform/create.blade.php
+++ resources/views/myform/create.blade.php
@@ -40,7 +40,7 @@
          </div>
 
         <div class="form-group">
-        <input type='checkbox' name='duplicate' {{ ($duplicate) ? 'checked' : '' }}><label for='duplicate'>* Duplicate</label>
+        <input type='checkbox' name='duplicate' id='duplicate' {{ ($duplicate) ? 'checked' : '' }}><label for='duplicate'>* Duplicate</label>
         </div>
         <p></p>
 
